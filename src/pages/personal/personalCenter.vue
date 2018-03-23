dc1b0f27471777444e33add8ffa23f5ab8999e15
guanhongyu <hongyu_guan@163.com>
2018-03-23 14:54:13 +0800

    icon

--- src/pages/personal/personalCenter.vue
+++ src/pages/personal/personalCenter.vue
@@ -38,8 +38,8 @@
                 </div>
                 <div class="litbox fl" @click="order(2)">
                     <div class="pic">
-                    <!-- <i class="iconfont icon-tupian"></i> -->
-                    <!-- <img src="../../assets/image/mycenter/usre3.png" /> -->
+                    <i class="iconfont icon-tupian"></i>
+                    <img src="../../assets/image/mycenter/usre3.png" />
                     <div class="number" v-if="numItem.noGetNum!=0&&numItem.noPayNum!=null">{{numItem.noGetNum}}</div></div>
                     <div class="font">待收货</div>
                 </div>
